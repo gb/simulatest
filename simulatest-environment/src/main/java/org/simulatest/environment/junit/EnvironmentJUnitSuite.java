a39b8d3b8ac593f0d7c544225b3dc5d95d8eb534
Gabriel Such <gabrielsuch@gmail.com>
2011-07-30 02:21:10 -0300

    Merge branch 'master' of https://gabrielsuch@github.com/gabrielsuch/simulatest.git
    Conflicts:
    	simulatest-environment/src/main/java/org/simulatest/environment/junit/AbstractEnvironmentJUnitRunner.java

--- simulatest-environment/src/main/java/org/simulatest/environment/junit/EnvironmentJUnitSuite.java
+++ simulatest-environment/src/main/java/org/simulatest/environment/junit/EnvironmentJUnitSuite.java
@@ -1,10 +1,8 @@
 package org.simulatest.environment.junit;
 
-
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 import org.simulatest.environment.environment.TestCaseRaker;
-
 
 public class EnvironmentJUnitSuite extends AbstractEnvironmentJUnitRunner {
 
