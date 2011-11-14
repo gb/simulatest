8c91a81e79f9ff40b8c29219c5ad2f0e34e93d60
Gabriel Such <gabrielsuch@gmail.com>
2011-11-14 18:17:27 -0200

    Testing the new constructor EnvironmentRunnerFacade(EnvironmentFactory environmentFactory)

--- simulatest-environment/src/test/java/org/simulatest/environment/test/EnvironmentRunnerFacadeTest.java
+++ simulatest-environment/src/test/java/org/simulatest/environment/test/EnvironmentRunnerFacadeTest.java
@@ -3,16 +3,18 @@ package org.simulatest.environment.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.simulatest.environment.environment.EnvironmentReflectionFactory;
 import org.simulatest.environment.environment.facade.EnvironmentRunnerFacade;
 import org.simulatest.environment.test.testdouble.DatabaseMock;
 import org.simulatest.environment.test.testdouble.environment.EnvironmentSecondLevel;
 
 public class EnvironmentRunnerFacadeTest {
 	
-	private EnvironmentRunnerFacade environmentRunnerFacade = new EnvironmentRunnerFacade();
+	private EnvironmentRunnerFacade environmentRunnerFacade;
 	
 	@Test
 	public void shouldRunAllLevelsOfTreeWhenIRunAnEnvironment() {
+		environmentRunnerFacade = new EnvironmentRunnerFacade();
 		environmentRunnerFacade.runEnvironment(EnvironmentSecondLevel.class);
 	
 		assertEquals(2, DatabaseMock.getMessages().size());
@@ -20,5 +22,14 @@ public class EnvironmentRunnerFacadeTest {
 		assertEquals("second", DatabaseMock.getMessages().get(1));
 	}
 	
+	@Test
+	public void testEnvironmentFacadeWithChooseOfRunner() {
+		environmentRunnerFacade = new EnvironmentRunnerFacade(new EnvironmentReflectionFactory());
+		environmentRunnerFacade.runEnvironment(EnvironmentSecondLevel.class);
+	
+		assertEquals(2, DatabaseMock.getMessages().size());
+		assertEquals("first", DatabaseMock.getMessages().get(0));
+		assertEquals("second", DatabaseMock.getMessages().get(1));
+	}
 
 }
