df9316086f5a27583b71e0381f6c0c0b63cf0a5a
Gabe Tobias <gabrielsuch@gmail.com>
2026-03-12 21:42:02 -0700

    refactor: remove insistencelayer-server module to focus on core features (#25)

--- simulatest-gui/src/main/java/org/simulatest/gui/SimulatestHub.java
+++ simulatest-gui/src/main/java/org/simulatest/gui/SimulatestHub.java
@@ -11,26 +11,23 @@ import net.miginfocom.swing.MigLayout;
 public class SimulatestHub extends JFrame {
 
 	private static final long serialVersionUID = 8209989456718369519L;
-	
+
 	private JButton sqlWindow;
 	private JButton environmentRunnerButton;
 	private JButton insistenceLayerButton;
-	private JButton insistenceLayerServerButton;
-	
+
 	private final EnvironmentRunner environmentRunner;
 	private final InsistenceLayerForm insistenceLayerForm;
 	private final SimulatestSQLWindow simulatestSQLWindow;
-	private final InsistenceLayerServerForm insistenceLayerServerForm;
 
 	public SimulatestHub(EnvironmentRunner environmentRunner, InsistenceLayerForm insistenceLayerForm,
-			SimulatestSQLWindow simulatestSQLWindow, InsistenceLayerServerForm insistenceLayerServerForm) {		
+			SimulatestSQLWindow simulatestSQLWindow) {
 		super("Simulatest Hub");
-		
+
 		this.environmentRunner = environmentRunner;
 		this.insistenceLayerForm = insistenceLayerForm;
 		this.simulatestSQLWindow = simulatestSQLWindow;
-		this.insistenceLayerServerForm = insistenceLayerServerForm;
-		
+
 		addComponents();
 		configureLayout();
 		createEvents();
@@ -38,21 +35,17 @@ public class SimulatestHub extends JFrame {
 
 	private void createEvents() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		insistenceLayerButton.addActionListener(new ActionListener() { @Override public void actionPerformed(ActionEvent e) {
 			insistenceLayerForm.showMe();
 		}});
-		
+
 		environmentRunnerButton.addActionListener(new ActionListener() { @Override public void actionPerformed(ActionEvent e) {
 			environmentRunner.setVisible(true);
 		}});
-		
+
 		sqlWindow.addActionListener(new ActionListener() { @Override public void actionPerformed(ActionEvent e) {
 			simulatestSQLWindow.showMe();
-		}});
-		
-		insistenceLayerServerButton.addActionListener(new ActionListener() { @Override public void actionPerformed(ActionEvent e) {
-			insistenceLayerServerForm.showMe();
 		}});
 	}
 
@@ -61,17 +54,15 @@ public class SimulatestHub extends JFrame {
 		this.add(sqlWindow);
 		this.add(environmentRunnerButton);
 		this.add(insistenceLayerButton);
-		this.add(insistenceLayerServerButton);
 		this.setResizable(false);
 		this.pack();
-		this.setLocationRelativeTo(null);		
+		this.setLocationRelativeTo(null);
 	}
 
 	private void addComponents() {
 		sqlWindow = new JButton("SQL Window");
-		environmentRunnerButton = new JButton("Environment Runner");	
+		environmentRunnerButton = new JButton("Environment Runner");
 		insistenceLayerButton = new JButton("Insistence Layer");
-		insistenceLayerServerButton = new JButton("Insistence Layer Server");
 	}
-	
+
 }
