78c5b62a73359850c50c0b03127e70c2a01378d0
gb <gabrielsuch@gmail.com>
2026-03-19 01:20:40 -0400

    refactor: rename simulatest-environment to simulatest-environment-junit4
    Naming was ambiguous next to simulatest-environment-core and
    simulatest-environment-junit5. Now all three are parallel.

---
+++ simulatest-environment-junit4/src/main/java/org/simulatest/environment/junit/SimulatestJUnit4ClassRunner.java
@@ -0,0 +1,48 @@
+package org.simulatest.environment.junit;
+
+import java.util.List;
+
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
+import org.simulatest.environment.plugin.SimulatestPlugin;
+import org.simulatest.environment.SimulatestSession;
+import org.simulatest.environment.infra.exception.EnvironmentInstantiationException;
+
+public class SimulatestJUnit4ClassRunner extends BlockJUnit4ClassRunner {
+
+	private final AbstractEnvironmentJUnitRunner runner;
+	private final List<SimulatestPlugin> plugins;
+
+	public SimulatestJUnit4ClassRunner(AbstractEnvironmentJUnitRunner runner, Class<?> clazz, List<SimulatestPlugin> plugins) throws InitializationError {
+		super(clazz);
+		this.runner = runner;
+		this.plugins = plugins;
+	}
+
+	@Override
+	protected Object createTest() throws Exception {
+		Object instance = SimulatestSession.createTestInstanceOrElse(plugins, getTestClass().getJavaClass(), this::newTestInstance);
+		SimulatestSession.postProcessWithPlugins(plugins, instance);
+		return instance;
+	}
+
+	private Object newTestInstance() {
+		try {
+			return super.createTest();
+		} catch (Exception e) {
+			throw new EnvironmentInstantiationException("Failed to create test instance: " + getTestClass().getName(), e);
+		}
+	}
+
+	@Override
+	protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+		try {
+			super.runChild(method, notifier);
+		} finally {
+			runner.resetInsistenceLevel();
+		}
+	}
+
+}
---
+++ simulatest-environment-junit4/src/test/java/org/simulatest/environment/test/EnvironmentExtractorTest.java
@@ -0,0 +1,75 @@
+package org.simulatest.environment.test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.simulatest.environment.annotation.UseEnvironment;
+import org.simulatest.environment.EnvironmentDefinition;
+import org.simulatest.environment.EnvironmentExtractor;
+import org.simulatest.environment.test.testdouble.Environments.PessoaEnvironment;
+import org.simulatest.environment.test.testdouble.Environments.Root;
+
+public class EnvironmentExtractorTest {
+	
+	private EnvironmentExtractor environmentExtractor;
+	private List<Class<?>> tests;
+	
+	@Before
+	public void setup() {
+		tests = new LinkedList<Class<?>>();
+		
+		tests.add(TestWithoutEnvironment1.class);
+		tests.add(TestWithoutEnvironment2.class);
+		
+		tests.add(TestWithEnvironment1.class);
+		tests.add(TestWithEnvironment2.class);
+		tests.add(TestWithEnvironment3.class);
+		tests.add(TestWithEnvironment4.class);
+		
+		environmentExtractor = EnvironmentExtractor.extract(tests);
+	}
+	
+	@Test
+	public void testEnvironmentMap() {
+		assertEquals(3, environmentExtractor.size());
+		
+		List<Class<?>> classesWithoutEnvironment = environmentExtractor.getTests(EnvironmentDefinition.bigBang());
+		
+		assertTrue(classesWithoutEnvironment.size() == 2);
+		assertTrue(classesWithoutEnvironment.contains(TestWithoutEnvironment1.class));
+		assertTrue(classesWithoutEnvironment.contains(TestWithoutEnvironment2.class));
+		
+		List<Class<?>> classWithRootEnvironment = environmentExtractor.getTests(EnvironmentDefinition.create(Root.class));
+		
+		assertTrue(classWithRootEnvironment.size() == 2);
+		assertTrue(classWithRootEnvironment.contains(TestWithEnvironment1.class));
+		assertTrue(classWithRootEnvironment.contains(TestWithEnvironment2.class));
+		
+		List<Class<?>> classesWithPessoaEnvironment = environmentExtractor.getTests(EnvironmentDefinition.create(PessoaEnvironment.class));
+		
+		assertTrue(classesWithPessoaEnvironment.size() == 2);
+		assertTrue(classesWithPessoaEnvironment.contains(TestWithEnvironment3.class));
+		assertTrue(classesWithPessoaEnvironment.contains(TestWithEnvironment4.class));
+	}
+		
+	class TestWithoutEnvironment1 { }
+	class TestWithoutEnvironment2 { }
+	
+	@UseEnvironment(Root.class)
+	class TestWithEnvironment1 { }
+	
+	@UseEnvironment(Root.class)
+	class TestWithEnvironment2 { }
+	
+	@UseEnvironment(PessoaEnvironment.class)
+	class TestWithEnvironment3 { }
+	
+	@UseEnvironment(PessoaEnvironment.class)
+	class TestWithEnvironment4 { }
+
+}
